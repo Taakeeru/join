70860a6af2c4e960a6902cf329bb9f18365fc2cf
A1leksD <aleks444@gmx.de>
2023-10-26 14:32:05 +0200

    added desktop_template
    incloude desktop_template

--- js/include.js
+++ js/include.js
@@ -1,11 +1,11 @@
-function includeHTML() {
+async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
         file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = fetch(file);
+        let resp = await fetch(file);
         if (resp.ok) {
-            element.innerHTML = resp.text();
+            element.innerHTML = await resp.text();
         } else {
             element.innerHTML = 'Page not found';
         }
