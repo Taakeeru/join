76bb9c42ac51d930176f81f19696c35babdf6d90
A1leksD <aleks444@gmx.de>
2023-11-02 14:08:24 +0100

    added a bit of js in contacts

---
+++ js/contacts.js
@@ -0,0 +1,53 @@
+let allContacts = [];
+let firstLetter = [];
+
+function init(){
+    // generateLatter();
+}
+
+function generateLatter(){
+    let letter = document.getElementById('generateLatter');
+    // letter.innerHTML = '';
+    for (let i = 0; i < firstLetter.length; i++) {
+        const letters = firstLetter[i];
+        letter.innerHTML = /*html*/`
+            <div class="positionLetterContact">
+                <p class="letterContact" id="letterContact">${letters}</p>
+            </div>
+            <div class="positionLineContact">
+                <p class="lineContact"></p>
+            </div>`;
+    }
+}
+
+function addNewContactBtn(){
+    document.getElementById('boxOfAddingNewContact').classList.toggle('d-none');
+    createNewContact();
+}
+
+
+/**
+ * create new contact vlaues and push it to the localStorage
+ */
+function createNewContact(){
+    const fullName = document.getElementById('nameAddContact').value;
+    const email = document.getElementById('emailAddContact').value;
+    const phone = document.getElementById('phoneAddContact').value;
+    allContacts.push({
+        "Neme": fullName,
+        "Email": email,
+        "Phone": phone
+    });
+    
+    let stringOfJSON = JSON.stringify(allContacts);
+    localStorage.setItem('fullContactInfo', stringOfJSON);
+}
+
+function closeAddContactWindow(){
+    document.getElementById('boxOfAddingNewContact').classList.toggle('d-none');
+}
+
+function closeAddContactBoxWithX(){
+    document.getElementById('boxOfAddingNewContact').classList.toggle('d-none');
+}
+
