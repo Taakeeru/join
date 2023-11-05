e2f1c4e96c92c7807fc9ceb2bb04beae833f4b6f
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-11-05 17:13:04 +0100

    join.login
    signup render function + some style changes

--- js/login.js
+++ js/login.js
@@ -1,23 +1,35 @@
 
 
 function renderSignUpContent() {
-    getElements();
-    let loginHeader = document.getElementById('login-header');
-
-    body.classList = 'bg-color';
-    privacy.classList = 'w-color';
-    legal.classList = 'w-color';
-    logo.innerHTML = '';
-    logo.innerHTML = `<img src="/assets/img/join-white.svg" alt="Logo Bild">`;
-    loginHeader.classList += ' d-none';
+    renderBody();
+    renderCard();
 }
 
-function getElements() {
+
+function renderBody() {
     let body = document.getElementById('body');
     let privacy = document.getElementById('privacy');
     let legal = document.getElementById('legal');
     let logo = document.getElementById('logo');
+    let loginHeader = document.getElementById('login-header');
     
+    body.classList.add('bg-color');
+    privacy.classList.add('w-color');
+    legal.classList.add('w-color');
+    logo.innerHTML = '';
+    logo.innerHTML = /* html */ `<img src="/assets/img/join-white.svg" alt="Logo Bild">`;
+    loginHeader.classList.add('d-none');
 }
 
 
+function renderCard() {
+    let name = document.getElementById('name');
+    let confirmPassword = document.getElementById('confirm-password');
+    let labelText = document.getElementById('label-text');
+    let guestButton = document.getElementById('guest-button');
+
+    name.classList.remove('d-none');
+    confirmPassword.classList.remove('d-none');
+    labelText.innerHTML = /* html */ `I accept the <a href="privacy.html">Privacy Policy</a>`
+    guestButton.classList.add('d-none');
+}
