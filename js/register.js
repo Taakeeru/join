fb1f352d8fe86ffc80c0cb65013dab69268df628
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-11-07 20:26:34 +0100

    join.login
    added new js files, implemented some functions for the registration process and changed some html structures for it

---
+++ js/register.js
@@ -0,0 +1,48 @@
+const uniqueId = generateUniqueId();
+let users = [];
+
+
+function generateUniqueId() {
+    const currentDate = new Date();
+    const timestamp = currentDate.getTime();
+    const randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  
+    const uniqueId = `${timestamp}${randomLetter}`;
+  
+    return uniqueId;
+}
+
+
+async function initRegister(){
+    register();
+}
+
+
+async function loadUsers(){
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch(e){
+        console.error('Loading error:', e);
+    }
+}
+
+
+async function register() {
+    registerBtn.disabled = true;
+
+    users.push({
+        id: uniqueId,
+        username: username.value,
+        email: email.value,
+        password: password.value,
+    });
+    await setItem('users', JSON.stringify(users));
+    resetForm();
+}
+
+
+function resetForm() {
+    email.value = '';
+    password.value = '';
+    registerBtn.disabled = false;
+}
