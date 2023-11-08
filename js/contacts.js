d238037b17ae9c1a9e08f8dbaa1ccd9c4865c17e
Aleks <aleks444@gmx.de>
2023-11-08 19:51:07 +0100

    added save function

--- js/contacts.js
+++ js/contacts.js
@@ -81,36 +81,9 @@ async function createNewContact() {
     fullName.value = '';
     email.value = '';
     phone.value = '';
+    document.getElementById('boxOfAddingNewContact').classList.toggle('d-none');
 }
 
-
-/** --- erstmal ignorieren
- * create new contact vlaues and push it to the backendStorage
- */
-// function createNewContact(){
-//     const fullName = document.getElementById('nameAddContact');
-//     const email = document.getElementById('emailAddContact');
-//     const phone = document.getElementById('phoneAddContact');
-//     let newContact = {
-//         'fullName': fullName.value.trim(),
-//         'email': email.value.trim(),
-//         'phone': phone.value.trim()
-//     };
-//     setItem('newContactData', newContact);
-
-//     fullName.value = '';
-//     email.value = '';
-//     phone.value = '';
-//     generateContactInSmall();
-
-//     // setItem('fullName', fullName.value.trim());
-//     // setItem('email', email.value.trim());
-//     // setItem('phone', phone.value.trim());
-//     // generateContactInSmall();
-//     // fullName.value = '';
-//     // email.value = '';
-//     // phone.value = '';
-// }
 
   
 function showDetailsOfContact(newPhone, newEmail, newName){  
@@ -142,6 +115,40 @@ function showDetailsOfContact(newPhone, newEmail, newName){
 
 function addNewContactBtn(){
     document.getElementById('boxOfAddingNewContact').classList.toggle('d-none');
+    document.getElementById('boxOfAddingNewContact').innerHTML = /*html*/`
+        <div>
+            <div class="boxOfAddingNewContact">
+                <div class="blueBoxAddContact">
+                <img src="../assets/img/capa1.svg" class="imgBlueBox">
+                    <h1 class="h1AddContact">Add Contact</h1>
+                    <p class="pAddContact">Tasks are better with a team!</p>
+                </div>
+                <img src="../assets/img/close.svg" class="closeAddContactBox" onclick="closeAddContactBoxWithX()">
+                <div class="witheBoxAddContact">
+                    <div class="detailsOFAddContact">
+                        <img src="../assets/img/head-663997_640.jpg" class="witheBoxAddContactImg">
+                        <div class="displayFlex">
+                            <input type="text" class="inputField" placeholder="Name" id="nameAddContact">
+                            <img src="../assets/img/person.svg" class="imgInInput">
+                        </div>
+                        <div class="displayFlex">
+                            <input type="text" class="inputField" placeholder="Email" id="emailAddContact">
+                            <img src="../assets/img/mail.svg" class="imgInInput">
+                        </div>
+                        <div class="displayFlex">
+                            <input type="text" class="inputField" placeholder="Phone" id="phoneAddContact">
+                            <img src="../assets/img/call.svg" class="imgInInput">
+                        </div>
+                        <div class="btnsAddContact">
+                            <button class="displayFlex btnCloseContact" onclick="closeAddContactWindow()">Close <img
+                                    src="../assets/img/vector.svg" class="samllIconsContactX"></button>
+                            <button class="displayFlex btnCreateContact" onclick="createNewContact()">Create contact
+                                <img src="../assets/img/check.svg" class="samllIconsContactOK"></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
 }
 
 
@@ -160,30 +167,32 @@ async function editContact(newName, newEmail, newPhone){
     let valueBox = document.getElementById('boxOfEdingContact');
     valueBox.innerHTML = '';
     valueBox.innerHTML = /*html*/`
-        <div class="blueBoxEditContact">
-            <img src="../assets/img/capa1.svg" class="imgBlueBox">
-            <h1 class="h1EditContact">Edit Contact</h1>
-        </div>
-        <img src="../assets/img/close.svg" class="closeEditContactBox" onclick="closeEditContactBox()">
-        <div class="witheBoxAddContact">
-            <div class="detailsOFAddContact">
-                <img src="../assets/img/head-663997_640.jpg" class="witheBoxAddContactImg">
-                <div class="displayFlex">
-                    <input type="text" class="inputField" placeholder="Name" id="nameEditContact">
-                    <img src="../assets/img/person.svg" class="imgInInput">
-                </div>
-                <div class="displayFlex">
-                    <input type="text" class="inputField" placeholder="Email" id="emailEditContact">
-                    <img src="../assets/img/mail.svg" class="imgInInput">
-                </div>
-                <div class="displayFlex">
-                    <input type="text" class="inputField" placeholder="Phone" id="phoneEditContact">
-                    <img src="../assets/img/call.svg" class="imgInInput">
-                </div>
-                <div class="btnsAddContact">
-                    <button class="displayFlex btnCloseContact" onclick="deleteContact('${newName}','${newEmail}','${newPhone}')">Delete</button>
-                    <button class="displayFlex btnCreateContact" onclick="saveEditContactWindow()">Save <img
-                            src="../assets/img/check.svg" class="samllIconsContactOK"></button>
+        <div class="boxOfEdingContact">
+            <div class="blueBoxEditContact">
+                <img src="../assets/img/capa1.svg" class="imgBlueBox">
+                <h1 class="h1EditContact">Edit Contact</h1>
+            </div>
+            <img src="../assets/img/close.svg" class="closeEditContactBox" onclick="closeEditContactBox()">
+            <div class="witheBoxAddContact">
+                <div class="detailsOFAddContact">
+                    <img src="../assets/img/head-663997_640.jpg" class="witheBoxAddContactImg">
+                    <div class="displayFlex">
+                        <input type="text" class="inputField" placeholder="Name" id="nameEditContact">
+                        <img src="../assets/img/person.svg" class="imgInInput">
+                    </div>
+                    <div class="displayFlex">
+                        <input type="text" class="inputField" placeholder="Email" id="emailEditContact">
+                        <img src="../assets/img/mail.svg" class="imgInInput">
+                    </div>
+                    <div class="displayFlex">
+                        <input type="text" class="inputField" placeholder="Phone" id="phoneEditContact">
+                        <img src="../assets/img/call.svg" class="imgInInput">
+                    </div>
+                    <div class="btnsAddContact">
+                        <button class="displayFlex btnCloseContact" onclick="deleteContact('${newName}','${newEmail}','${newPhone}')">Delete</button>
+                        <button class="displayFlex btnCreateContact" onclick="saveEditContactWindow('${newName}','${newEmail}','${newPhone}')">Save <img
+                                src="../assets/img/check.svg" class="samllIconsContactOK"></button>
+                    </div>
                 </div>
             </div>
         </div>`;
@@ -219,5 +228,28 @@ async function deleteContact(newName, newEmail, newPhone) {
     await setItem('newContactData', JSON.stringify(storedContacts));
     await generateContactInSmall();
     document.getElementById('boxOfDetailsContacts').classList.toggle('d-none');
-    document.getElementById('boxOfEdingContact').classList.toggle('d-none');
+    document.getElementById('boxOfEdingContact').classList.add('d-none');
+}
+
+
+async function saveEditContactWindow(newName, newEmail, newPhone) {
+    const response = await getItem('newContactData');
+    const storedContacts = JSON.parse(response.data.value);
+
+    for (const key in storedContacts) {
+        if (
+            storedContacts[key].fullName === newName &&
+            storedContacts[key].email === newEmail &&
+            storedContacts[key].phone === newPhone
+        ) {
+            storedContacts[key].fullName = document.getElementById('nameEditContact').value.trim();
+            storedContacts[key].email = document.getElementById('emailEditContact').value.trim();
+            storedContacts[key].phone = document.getElementById('phoneEditContact').value.trim();
+            break;
+        }
+    }
+
+    await setItem('newContactData', JSON.stringify(storedContacts));
+    await generateContactInSmall();
+    document.getElementById('boxOfEdingContact').classList.add('d-none');
 }
