82144809bf04b391846fcc6acd006125b59a490c
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-11-09 03:24:04 +0100

    join.summary
    added a function to show realtime date

--- js/summary.js
+++ js/summary.js
@@ -4,4 +4,14 @@ async function init() {
     loggedInUser = await getLoggedInUser();
     greetUser(loggedInUser);
     showProfileInitials(loggedInUser);
+    updateCurrentDate();
+}
+
+
+function updateCurrentDate() {
+    const date = document.getElementById('date');
+    const currentDate = new Date();
+    const options = { month: 'long', day: 'numeric', year: 'numeric' };
+    const formattedDate = currentDate.toLocaleDateString('en-EN', options);
+    date.textContent = formattedDate;
 }
