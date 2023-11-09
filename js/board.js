ffb943217762d3284dbcf50e0281b2d870730b7c
HomeBjo <122940319+HomeBjo@users.noreply.github.com>
2023-11-09 15:49:57 +0100

    prettier

--- js/board.js
+++ js/board.js
@@ -1,85 +1,92 @@
-let todos = [{
-    'id': 0,
-    'title': 'Ich',
-    'category': 'toDo'
-}, {
-    'id': 1,
-    'title': 'Du',
-    'category': 'toDo'
-}, {
-    'id': 2,
-    'title': 'Sie',
-    'category': 'progress'
-}, {
-    'id': 3,
-    'title': 'Er',
-    'category': 'feedBack'
-}, {
-    'id': 4,
-    'title': 'Wir',
-    'category': 'done'
-}, {
-    'id': 5,
-    'title': 'Alle',
-    'category': 'done'
-}];
+let todos = [
+  {
+    id: 0,
+    title: "Ich",
+    category: "toDo",
+  },
+  {
+    id: 1,
+    title: "Du",
+    category: "toDo",
+  },
+  {
+    id: 2,
+    title: "Sie",
+    category: "progress",
+  },
+  {
+    id: 3,
+    title: "Er",
+    category: "feedBack",
+  },
+  {
+    id: 4,
+    title: "Wir",
+    category: "done",
+  },
+  {
+    id: 5,
+    title: "Alle",
+    category: "done",
+  },
+];
 async function init() {
-    includeHTML();
-    updateHTML();
-    loggedInUser = await getLoggedInUser();
-    showProfileInitials(loggedInUser);
+  includeHTML();
+  updateHTML();
+  loggedInUser = await getLoggedInUser();
+  showProfileInitials(loggedInUser);
 }
 
 let currentDraggedElement;
 
 function updateHTML() {
-    let toDolist = todos.filter(t => t['category'] == 'toDo');
+  let toDolist = todos.filter((t) => t["category"] == "toDo");
 
-    document.getElementById('toDo').innerHTML = '';
+  document.getElementById("toDo").innerHTML = "";
 
-    for (let index = 0; index < toDolist.length; index++) {
-        const element = toDolist[index];
-        document.getElementById('toDo').innerHTML += generateTodoHTML(element);
-    }
+  for (let index = 0; index < toDolist.length; index++) {
+    const element = toDolist[index];
+    document.getElementById("toDo").innerHTML += generateTodoHTML(element);
+  }
 
-    let progressList = todos.filter(t => t['category'] == 'progress');
+  let progressList = todos.filter((t) => t["category"] == "progress");
 
-    document.getElementById('progress').innerHTML = '';
+  document.getElementById("progress").innerHTML = "";
 
-    for (let index = 0; index < progressList.length; index++) {
-        const element = progressList[index];
-        document.getElementById('progress').innerHTML += generateTodoHTML(element);
-    }
+  for (let index = 0; index < progressList.length; index++) {
+    const element = progressList[index];
+    document.getElementById("progress").innerHTML += generateTodoHTML(element);
+  }
 
-    let feedbackList = todos.filter(t => t['category'] == 'feedBack');
+  let feedbackList = todos.filter((t) => t["category"] == "feedBack");
 
-    document.getElementById('feedBack').innerHTML = '';
+  document.getElementById("feedBack").innerHTML = "";
 
-    for (let index = 0; index < feedbackList.length; index++) {
-        const element = feedbackList[index];
-        document.getElementById('feedBack').innerHTML += generateTodoHTML(element);
-    }
+  for (let index = 0; index < feedbackList.length; index++) {
+    const element = feedbackList[index];
+    document.getElementById("feedBack").innerHTML += generateTodoHTML(element);
+  }
 
-    let doneList = todos.filter(t => t['category'] == 'done');
+  let doneList = todos.filter((t) => t["category"] == "done");
 
-    document.getElementById('done').innerHTML = '';
+  document.getElementById("done").innerHTML = "";
 
-    for (let index = 0; index < doneList.length; index++) {
-        const element = doneList[index];
-        document.getElementById('done').innerHTML += generateTodoHTML(element);
-    }
+  for (let index = 0; index < doneList.length; index++) {
+    const element = doneList[index];
+    document.getElementById("done").innerHTML += generateTodoHTML(element);
+  }
 }
 
 function startDragging(id) {
-    currentDraggedElement = id;
+  currentDraggedElement = id;
 }
 
 function generateTodoHTML(element) {
-    return `
-    <div class="cards" draggable="true" ondragstart="startDragging(${element['id']})">
+  return `
+    <div class="cards" draggable="true" ondragstart="startDragging(${element["id"]})">
               <div class="cardContent">
                 <div class="cardHeader">
-                  <p class="userStory">${element['title']}</p>
+                  <p class="userStory">${element["title"]}</p>
                 </div>
                 <div class="cardDescription">
                   <p class="cardDescriptionHeader">Contact Form & Imprint</p>
@@ -102,47 +109,33 @@ function generateTodoHTML(element) {
 }
 
 function allowDrop(ev) {
-    ev.preventDefault();
+  ev.preventDefault();
 }
 
 function moveTo(category) {
-    todos[currentDraggedElement]['category'] = category;
-    updateHTML();
+  todos[currentDraggedElement]["category"] = category;
+  updateHTML();
 }
 
 function highlight(id) {
-    document.getElementById(id).classList.add('boardtoDoSektion-highlight');
+  document.getElementById(id).classList.add("boardtoDoSektion-highlight");
 }
 
 function removeHighlight(id) {
-    
-    setTimeout(function() {
-        document.getElementById(id).classList.remove('boardtoDoSektion-highlight');
-    }, 100); 
+  setTimeout(function () {
+    document.getElementById(id).classList.remove("boardtoDoSektion-highlight");
+  }, 100);
 }
 
 function showAddTaskMenu() {
-  document.getElementById('menuContainerBox').classList.add('menuContainer');
-  document.getElementById('sideMenu').classList.add('showmenu');
+  document.getElementById("menuContainerBox").classList.add("menuContainer");
+  document.getElementById("sideMenu").classList.add("showmenu");
 }
 
 function closeAddTaskMenu() {
-  document.getElementById('menuContainerBox').classList.remove('menuContainer');
-  document.getElementById('sideMenu').classList.remove('showmenu');
+  document.getElementById("menuContainerBox").classList.remove("menuContainer");
+  document.getElementById("sideMenu").classList.remove("showmenu");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*<div class="cards">
               <div class="cardContent">
