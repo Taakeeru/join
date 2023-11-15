ea61723e2c98ca4739a8ae3fa7ba1bf893695b68
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-11-15 07:21:10 +0100

    join.legal
    added a mini extra function to prevent privacy for non logged in users

--- js/legal.js
+++ js/legal.js
@@ -12,8 +12,10 @@ function removePrivateElements() {
     let help = document.getElementById('helpImg');
     let profile = document.getElementById('profile-div');
     let aside = document.getElementById('aside');
+    let headerTitle = document.getElementById('header-title');
 
     help.classList.add('d-none');
     profile.classList.add('d-none');
     aside.classList.add('d-none');
+    headerTitle.onclick = null;
 }
