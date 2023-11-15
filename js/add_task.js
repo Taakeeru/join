9d55986f03da9f6d020d3d8ad297ce486ece2c94
HomeBjo <122940319+HomeBjo@users.noreply.github.com>
2023-11-15 14:33:05 +0100

    add required Text

--- js/add_task.js
+++ js/add_task.js
@@ -5,3 +5,30 @@ async function initAddTask() {
     showProfileInitials(loggedInUser);
     loadUsers();
 }
+
+function checkInputFields() {
+    let titleInput = document.getElementById('title-fail');
+    let descriptionInput = document.getElementById('description-fail');
+    let dateInput = document.getElementById('date-fail');
+  
+    let titleFail = document.getElementById('tile-fail-message');
+    let descriptionFail = document.getElementById('description-fail-message');
+    let dateFail = document.getElementById('date-fail-message');
+  
+    titleFail.innerHTML = '';
+    descriptionFail.innerHTML = '';
+    dateFail.innerHTML = '';
+  
+    if (titleInput.value.trim() === '') {
+      titleFail.innerHTML = '<span>Title is required</span>';
+    }
+  
+    if (descriptionInput.value.trim() === '') {
+      descriptionFail.innerHTML = '<span>Description is required</span>';
+    }
+  
+    if (dateInput.value.trim() === '') {
+      dateFail.innerHTML = '<span>Due date is required</span>';
+    }
+  }
+  
