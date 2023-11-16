c31540a44eb39cbfd2843a4e9d16c358b19d8f0d
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-11-16 14:44:54 +0100

    join.legal
    added new files and changed the content

--- js/legal.js
+++ js/legal.js
@@ -4,18 +4,6 @@
 async function initLegal() {
     loggedInUser = await getLoggedInUser();
     showProfileInitials(loggedInUser);
-    removePrivateElements();
 }
 
 
-function removePrivateElements() {
-    let help = document.getElementById('helpImg');
-    let profile = document.getElementById('profile-div');
-    let aside = document.getElementById('aside');
-    let headerTitle = document.getElementById('header-title');
-
-    help.classList.add('d-none');
-    profile.classList.add('d-none');
-    aside.classList.add('d-none');
-    headerTitle.onclick = null;
-}
