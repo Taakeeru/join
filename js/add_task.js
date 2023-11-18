d64b111f1ef780e820a0a7b1d9b64e41c77f5a2c
Aleks <143895611+A1eksD@users.noreply.github.com>
2023-11-18 21:19:38 +0100

    changed add task functions a bit

--- js/add_task.js
+++ js/add_task.js
@@ -1,4 +1,5 @@
 let allTasks =[];
+let contactData = [];
 
 async function initAddTask() {
   await includeHTML();
@@ -18,85 +19,57 @@ async function getAllTasks() {
       console.error('Loading error:', e);
   }
 }
-// async function createNewTask() {
-//   let getTitle = document.getElementById('addTastTitel').value;
-//   let getDescription = document.getElementById('addTastTextArea').value;
-//   let getDateValue = document.getElementById('dueDateValue').value;
 
-//   getTitle = getTitle.trim(); // Ensure title is not empty
-
-//   const existingTaskIndex = allTasks.findIndex(task => task.title === getTitle);
-
-//   if (existingTaskIndex === -1) {
-//     let newTask = ({
-//       title: getTitle,
-//       description: getDescription,
-//       date: getDateValue,
-//     });
-//     allTasks.push(newTask);
-//     console.log(allTasks);
-//   } else {
-//     alert('Task bereits vorhanden');
-//   }
-//   // Update the local storage with the modified array
-//   await setItem('newTask', JSON.stringify(allTasks));
-// }
 
 async function createNewTask(){
   let getTitel = document.getElementById('addTastTitel').value;
   let getTextArea = document.getElementById('addTastTextArea').value;
-  let getDateValue = document.getElementById('dueDateValue').value; // date muss vermutlich überarbeitet werden
-  let getPriority = getThePriority();
-  // let seeContacts = await getItem('users', loggedInUser.contacts);
+  let getDateValue = document.getElementById('dueDateValue').value;
+  // let getPriority = getThePriority(priority);
+  // let contactData = await showAssignetContacts(loggedInUser);
   // let assignetTo = JSON.parse(seeContacts);
   let getCategory = loadCategory(); 
-  let getSubtask = addedSubtask();
-  await pushTaskInfo(getTitel, getTextArea, getDateValue, getPriority, getCategory, getCategory, getSubtask);
-  
+  // let getSubtask = addedSubtask();
+  await pushTaskInfo(getTitel, getTextArea, getDateValue, selectedUsers, getCategory);
 }
 
-function getThePriority(priority) {
-  selectedPriority = priority;
-  return selectedPriority;
+function clearArray(){
+  allTasks.splice(0, allTasks.length);
 }
 
-async function pushTaskInfo(getTitle, getDescription, getDateValue, getPriority, getCategory, getCategory, getSubtask) {
-  getTitle = getTitle.trim(); // Ensure title is not empty
+
+async function pushTaskInfo(getTitle, getDescription, getDateValue, contactData, getCategory) {
+  getTitle = getTitle.trim(); 
   const existingTaskIndex = allTasks.findIndex(task => task.title === getTitle);
 
   if (existingTaskIndex === -1) {
-    let newTask = ({
+    let newTask = {
       id: allTasks.length, 
       title: getTitle,
       description: getDescription,
-      priority: getPriority,
+      // priority: getPriority,
       date: getDateValue,
+      contacts: contactData,
       workCategory: getCategory,
-      subtask: getSubtask,
       category: "toDo"
-    });
+    };
     allTasks.push(newTask);
     console.log(allTasks);
     alert('Task angelegt');
   } else {
     alert('Task bereits vorhanden');
   }
-  // Update the local storage with the modified array
-  await setItem('newTask', JSON.stringify(allTasks));
+  // await setItem('newTask', JSON.stringify(allTasks));
 }
 
 
-
-// ---------------------------------------Remote
 function checkInputFields() {
     let titleInput = document.getElementById('addTastTitel');
     let descriptionInput = document.getElementById('addTastTextArea');
     let dateInput = document.getElementById('dueDateValue');
-  
     let titleFail = document.getElementById('tile-fail-message');
     let descriptionFail = document.getElementById('description-fail-message');
     let dateFail = document.getElementById('date-fail-message');
-  
     titleFail.innerHTML = '';
     descriptionFail.innerHTML = '';
     dateFail.innerHTML = '';
@@ -119,65 +92,86 @@ function checkInputFields() {
     box.classList.toggle("d-none");
   }
 
-function showAssignetContacts(loggedInUser) {
-  let box = document.getElementById("selectContainer");
+  async function showAssignetContacts(loggedInUser) {
+    let box = document.getElementById("selectContainer");
   
-  // let getUser = await getItem('loggedInUser', JSON.parse(loggedInUser.contacts));
-  for (let i = 0; i < loggedInUser.contacts.length; i++) {
-    let userName = loggedInUser.contacts[i].name;
-    let getInitial = loggedInUser.contacts[i].initial;
-    let getColor = loggedInUser.contacts[i].color;
-
-    box.innerHTML += /*html*/`
-      <div class="userBoxContainer displayFlex">
-        <div class="imgPerson displayFlex" style="background-color: ${getColor};">${getInitial}</div>
-        <span class="userPosition">${userName}</span>
-        <input type="checkbox" id="inputId${i}" onclick="handleCheckboxClick('${i}', '${userName}', '${getInitial}', '${getColor}')">
-      </div>`;
+    for (let i = 0; i < loggedInUser.contacts.length; i++) {
+      let userName = loggedInUser.contacts[i].name;
+      let getInitial = loggedInUser.contacts[i].initial;
+      let getColor = loggedInUser.contacts[i].color;
+      box.innerHTML += /*html*/`
+        <div class="userBoxContainer displayFlex">
+          <div class="imgPerson displayFlex" style="background-color: ${getColor};">${getInitial}</div>
+          <span class="userPosition">${userName}</span>
+          <input type="checkbox" id="inputId${i}" onclick="handleCheckboxClick('${i}', '${userName}', '${getInitial}', '${getColor}')">
+        </div>`;
+  
+      let userContactData = {
+        name: userName,
+        email: loggedInUser.contacts[i].email,
+        phone: loggedInUser.contacts[i].phone,
+        initial: getInitial,
+        color: getColor
+      };
+      
+      contactData.push(userContactData);
+    }
+  
+    return contactData;
   }
-}
+  
 
-function handleCheckboxClick(i, userName, getInitial, getColor) {
+  let selectedUsers = [];
 
-  let checkbox = document.getElementById(`inputId${i}`);
-  let addUser = document.getElementById("addContactstoassign");
-  let userId = `user_${i}`;
-
-  if (checkbox.checked) {
-    addUser.innerHTML += `
-      <div id="${userId}" class="userBoxContainer displayFlex">
-        <div class="imgPerson displayFlex" style="background-color: ${getColor};">${getInitial}</div>
-      </div>`;
-    updateHTML(i, userName, getInitial, getColor); //-----
-  } else {
-    let userToRemove = document.getElementById(userId);
-    if (userToRemove) {
-      userToRemove.remove();
+  function handleCheckboxClick(i, userName, getInitial, getColor) {
+    let checkbox = document.getElementById(`inputId${i}`);
+    let addUser = document.getElementById("addContactstoassign");
+    let userId = `user_${i}`;
+  
+    if (checkbox.checked) {
+      addUser.innerHTML += `
+        <div id="${userId}" class="userBoxContainer displayFlex">
+          <div class="imgPerson displayFlex" style="background-color: ${getColor};">${getInitial}</div>
+        </div>`;
+  
+      let selectedUser = {
+        name: userName,
+        email: loggedInUser.contacts[i].email,
+        phone: loggedInUser.contacts[i].phone,
+        initial: getInitial,
+        color: getColor
+      };
+  
+      selectedUsers.push(selectedUser);
+    } else {
+      let userToRemove = document.getElementById(userId);
+      if (userToRemove) {
+        userToRemove.remove();
+        selectedUsers = selectedUsers.filter(user => user.name !== userName);
+      }
     }
   }
-}
+  
 
-  function closeSelectContainer(event) {
+
+function closeSelectContainer(event) {
   let selectContainer = document.getElementById("selectContainer");
   let assignedSelect = document.getElementById("assignedSelect");
-
-  if (!assignedSelect.contains(event.target) && !selectContainer.contains(event.target)) {
-    
+  if (!assignedSelect.contains(event.target) && !selectContainer.contains(event.target)) { 
     selectContainer.classList.add("d-none");
   }
 }
 
+
 function showCategoryContacts() {
   document.getElementById("categoryContainer").classList.toggle("d-none");
-
 }
+
 
 function closeCategoryContainer(event) {
   let selectContainer = document.getElementById("categoryContainer");
   let assignedSelect = document.getElementById("categorySelect");
-
   if (!assignedSelect.contains(event.target) && !selectContainer.contains(event.target)) {
-    
     selectContainer.classList.add("d-none");
   }
 }
@@ -187,7 +181,6 @@ function addSubTask() {
   let addTask =  document.getElementById('subtaskContainer');
   let subtaskValue = subtaskInput.value;
   let subtaskId = 'subtask' + Date.now();
-  
   if (subtaskValue.trim() !== '') { 
     addTask.innerHTML += /*html*/`
       <ul>
@@ -214,7 +207,6 @@ function addSubTask() {
 function loadTechnicalTask() {
   let Box = document.getElementById('technicalTaskID');
   let currentValue = Box.innerHTML;
-
   if (currentValue === "Technical Task") {
     document.getElementById('SelectTaskCatergory').innerHTML = currentValue;
   }
@@ -224,7 +216,6 @@ function loadTechnicalTask() {
 function loadUserStory(){
   let Box = document.getElementById('userStoryID');
   let currentValue = Box.innerHTML;
-
   if (currentValue === "User Story") {
     document.getElementById('SelectTaskCatergory').innerHTML = currentValue;
   }
@@ -246,7 +237,7 @@ function addedSubtask(){
 function editSubtask(subtaskId) {
   let editedValue = document.getElementById(subtaskId).textContent;
   console.log('Edit Subtask:', editedValue);
-  // Füge hier den Code für die Bearbeitung des Subtasks hinzu
+  
 }
 
 
